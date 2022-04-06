ba9bdabaf71fc9e66fa3c025bb5b6d47d04a2783
AkashPathak99 <akash30.pathak@gmail.com>
2022-04-06 17:28:07 +0000

    initial commit

---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {MatInputModule} from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatInputModule
+    
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
